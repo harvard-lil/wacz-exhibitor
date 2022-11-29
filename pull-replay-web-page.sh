c3d20d256ebe67b4110c979c635a340be02b0317
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2022-11-29 10:40:37 -0500

    Upgrade to replayweb.page 1.7.6

--- pull-replay-web-page.sh
+++ pull-replay-web-page.sh
@@ -1,3 +1,3 @@
 # Pulls the latest version of `<replay-web-page>` (https://replayweb.page)
-curl https://cdn.jsdelivr.net/npm/replaywebpage@1.7.2/sw.js > html/replay-web-page/sw.js
-curl https://cdn.jsdelivr.net/npm/replaywebpage@1.7.2/ui.js > html/replay-web-page/ui.js
+curl https://cdn.jsdelivr.net/npm/replaywebpage@1.7.6/sw.js > html/replay-web-page/sw.js
+curl https://cdn.jsdelivr.net/npm/replaywebpage@1.7.6/ui.js > html/replay-web-page/ui.js
